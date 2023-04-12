9e957089fa709b0f25c5b71da6b135c8baf27408
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-04-13 00:27:03 +0300

    Task 2 has been completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player'; // API плеера находится в сущности Player
+import throttle from 'lodash.throttle'; // тротлинг из лоудаша
+import storage from './storage'; // local storage
+// ^ здесь деструктуризация { save, load } не работает
 
+const { save, load } = storage; // Ок, выделим память и деструктуризируем тут
+
+const iframe = document.querySelector('iframe'); // Нашли плеер
+const player = new Player(iframe); // Выделяем экземпляр сущности для API плеера
+const VIDPLAYER_CURTIME = 'videoplayer-current-time'; // Константа-ключ для работы с local storage
+
+function onTimeUpdate(event) {
+  save(VIDPLAYER_CURTIME, event.seconds); // Запись в local storage
+  console.log(`time updated! ${event.seconds}`);
+}
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000)); // По умолчанию раз в 250мс, но тротлинг лоудаша замедляет до 1000мс
+
+// При перезагрузке страницы
+window.onload = () => {
+  player.setCurrentTime(load(VIDPLAYER_CURTIME)); // Вспоминаем предыдущий таймлайн
+  console.log(`time loaded!`, load(VIDPLAYER_CURTIME));
+};
