563360088b59d534e8b5d6ecdf8adbb649d0fefc
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-04-13 01:00:33 +0300

    Task 3 has been completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+import storage from './storage';
 
+const { save, load } = storage; // Модуль для работы с local storage
+const FORMSTATE_KEY = 'feedback-form-state'; // Ключ для local storage
+
+const formEl = document.querySelector('form');
+const emailEl = document.querySelector('[name=email]');
+const textAreaEl = document.querySelector('[name=message]');
+
+function onInput() {
+  save(FORMSTATE_KEY, { email: emailEl.value, message: textAreaEl.value });
+  // Это мы запоминаем
+}
+
+function onSubmit(event) {
+  event.preventDefault(); // Отменяем действие по умолчанию
+
+  console.log({ email: emailEl.value, message: textAreaEl.value }); // Объект в консоль
+
+  // Очищаем поля формы
+  emailEl.value = '';
+  textAreaEl.value = '';
+
+  save(FORMSTATE_KEY, { email: emailEl.value, message: textAreaEl.value });
+  // Это мы забываем
+}
+
+window.onload = () => {
+  const { email, message } = load(FORMSTATE_KEY); // Загружаем из хранилища
+  if (email) {
+    emailEl.value = email; // Почту
+  }
+  if (message) {
+    textAreaEl.value = message; // Текст сообщения
+  }
+};
+
+formEl.addEventListener('keydown', throttle(onInput, 500)); // Не чаще раза в 500мс
+formEl.addEventListener('submit', onSubmit);
