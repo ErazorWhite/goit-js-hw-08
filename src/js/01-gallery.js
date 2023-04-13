6ab1e8a125e31b2118d4ccc97db9e85600f824b1
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-04-13 11:48:49 +0300

    1 task hotfix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,20 +4,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// Чтобы не вносить изменения в HTML добавляем basicLightBox при помощи JS
-const head = document.querySelector("head"); // выбираем элемент head на странице
-const link = document.createElement("link"); // создаем элемент link
-link.rel = "stylesheet"; // задаем атрибут rel
-link.href =
-  "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.13.0/simple-lightbox.min.css"; // задаем ссылку на файл стилей
-head.appendChild(link); // добавляем элемент link в head
-
-const body = document.querySelector("body"); // выбираем элемент body на странице
-const script = document.createElement("script"); // создаем элемент script
-script.src =
-  "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.13.0/simple-lightbox.min.js"; // задаем ссылку на файл скрипта
-body.appendChild(script); // добавляем элемент script в body
-
 // Создаёт разметку по шаблону из gallery-items.js
 function createGalleryMarkup(galleryItems) {
   return galleryItems
@@ -32,18 +18,15 @@ function createGalleryMarkup(galleryItems) {
         </a>
     </li>`;
     })
-    .join("");
+    .join('');
 }
 
-const galleryEl = document.querySelector(".gallery"); // Выбираем наш список с фото из галереи
-galleryEl.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems)); // Закидываем туда разметку с фотками из шаблона
+const galleryEl = document.querySelector('.gallery'); // Выбираем наш список с фото из галереи
+galleryEl.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems)); // Закидываем туда разметку с фотками из шаблона
 
-script.onload = function () {
-  // ждем, пока скрипт загрузится
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captions: true, // Включаем подписи
-    captionPosition: "bottom", // Внизу
-    captionDelay: 250, // Появляются через 250мс
-    captionsData: "alt", // Текст берут из img alt атрибута
-  });
-};
+new SimpleLightbox('.gallery a', {
+  captions: true, // Включаем подписи
+  captionPosition: 'bottom', // Внизу
+  captionDelay: 250, // Появляются через 250мс
+  captionsData: 'alt', // Текст берут из img alt атрибута
+});
